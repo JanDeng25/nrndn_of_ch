c125ade045e4c15539f8e315c1df0317a7292a42
Jan Deng <1003932334@qq.com>
2016-03-08 18:30:19 +0800

    adjust std::map<uint32_t,std::string> interestSent: string to std::string

--- ns-3/scratch/nrndn_ch/nrConsumer.h
+++ ns-3/scratch/nrndn_ch/nrConsumer.h
@@ -111,7 +111,7 @@ private:
 
 	  uint32_t m_virtualPayloadSize;
 	  Name m_prefix;
-	  std::map<uint32_t,string> interestSent;
+	  std::map<uint32_t,std::string> interestSent;
 	  std::map<uint32_t, double> msgTime;
 	  string m_oldLane;
 
